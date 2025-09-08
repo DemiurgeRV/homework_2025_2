cbcf6b10c2bbae571cbbc191c1ca78f1899b62de
Rybin Vladislav <rybinvladislav@mail.ru>
2025-09-08 15:26:55 +0300

    Added func planify

---
+++ source/plainify.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * Преобразует вложенный объект в plain-объект с ключами в формате "a.b.c".
+ *
+ * @param {Object} obj - Исходный объект с вложенными свойствами
+ * @returns {Object} plain-объект, где ключи представляют путь к значению
+ *
+ * @example
+ * plainify({ a: 1, b: { c: 2 } });
+ * // returns { a: 1, 'b.c': 2 }
+ */
+
+function plainify(obj) {
+    const result = {};
+    const stack = [{ value: obj, path: [] }];
+
+    while (stack.length > 0) {
+        const { value, path } = stack.pop();
+
+        for (const key in value) {
+            if (!value.hasOwnProperty(key)) continue;
+
+            const newPath = [...path, key];
+            const newKey = newPath.join('.');
+            const currentValue = value[key];
+
+            if (currentValue !== null && typeof currentValue === 'object' && !Array.isArray(currentValue)) {
+                stack.push({ value: currentValue, path: newPath });
+            } else {
+                result[newKey] = currentValue;
+            }
+        }
+    }
+
+    return result;
+}
